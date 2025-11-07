bac348a53e16a6325d832a670566c443daf4b02e
Fabi01a <gonzalezfabs@gmail.com>
2025-11-06 19:37:50 -0700

    uncommented another line to continue testing -> view tables

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-# from app.routes import routes
+from app.routes import routes
 
 #create db instance globally
 db = SQLAlchemy()
