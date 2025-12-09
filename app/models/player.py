6b770d29311ef50e75704806ed15f8737eb4aa41
Fabi01a <gonzalezfabs@gmail.com>
2025-12-09 14:12:11 -0700

    updated current migration

--- app/models/player.py
+++ app/models/player.py
@@ -3,6 +3,7 @@ from app.extensions import db
 class Player(db.Model):
     player_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     player_name = db.Column(db.String(20), unique=True, nullable=False)
+    normalized_name = db.Column(db.String(20), unique=True, nullable=False)
     game_sessions = db.relationship('GameSession', backref='player')
 
     def __repr__(self):
--- app/services/player_service.py
+++ app/services/player_service.py
@@ -2,12 +2,12 @@ from app.models.player import Player
 from app import db
 
 def get_or_create_player(player_name: str) -> Player:
-    player = Player.query.filter_by(player_name=player_name).first()
-    
+    normalized_name = player_name.lower()
+    player = Player.query.filter_by(normalized_name=normalized_name).first()
     if player:
         return player, True
     
-    player = Player(player_name=player_name)
+    player = Player(player_name=player_name, normalized_name=normalized_name)
     db.session.add(player)
     db.session.commit()
     return player, False
